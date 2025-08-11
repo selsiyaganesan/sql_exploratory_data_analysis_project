48e403075dcb4f6ae93f62b9a21c65fe49b4d2d5
selsiyaganesan <selsiyaganesan@gmail.com>
2025-08-11 23:35:55 +0530

    Update measures_exploration.sql

--- scripts/measures_exploration.sql
+++ scripts/measures_exploration.sql
@@ -15,30 +15,37 @@ SQL Functions Used:
 SELECT
     SUM(sales_amount) AS Total_sales
 from Gold.fact_sales
+    
 --find how many items are sold
 SELECT
     COUNT(quantity) AS items_sold
 FROM Gold.fact_sales
+    
 --find the average selling price
 SELECT
     AVG(price) AS avg_selling_price
 FROM Gold.fact_sales
+    
 --Find the total number of orders
 SELECT
     COUNT(DISTINCT(order_number)) AS total_nr_of_orders
 from Gold.fact_sales
+    
 --find the total number of products
 SELECT
     COUNT(product_name) AS total_nr_of_products
 FROM Gold.dim_product
+    
 --find the total number of customer
 SELECT 
     COUNT(customer_id) AS total_nr_of_customers
 FROM Gold.dim_customer
+    
 --find the total number of customers that has placed the orders
 SELECT
     COUNT(DISTINCT(customer_key)) AS placed_order
 FROM Gold.fact_sales
+    
 --general combined measure
 SELECT 'Total_sales' as measure_name ,SUM(sales_amount) AS measure_value from Gold.fact_sales
 UNION ALL 
